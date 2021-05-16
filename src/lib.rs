0f746d93f219a624e4dfaef1cdd1799e775d4166
jorrit.salverda@gmail.com <jsalverda@travix.com>
2021-05-16 12:16:18 +0200

    move shared models to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,67 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, PartialEq, Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub enum EntityType {
+    #[serde(rename = "")]
+    Invalid,
+    #[serde(rename = "ENTITY_TYPE_TARIFF")]
+    Tariff,
+    #[serde(rename = "ENTITY_TYPE_ZONE")]
+    Zone,
+    #[serde(rename = "ENTITY_TYPE_DEVICE")]
+    Device,
 }
 
-struct Measurement {
-	ID:             String,
-	Source:         String,
-	Location:       String,
-	Samples:        Vec<Sample>,
-	MeasuredAtTime: std::time::SystemTime,
+#[derive(Copy, Clone, PartialEq, Serialize, Deserialize, Debug)]
+pub enum MetricType {
+    #[serde(rename = "")]
+    Invalid,
+    #[serde(rename = "METRIC_TYPE_COUNTER")]
+    Counter,
+    #[serde(rename = "METRIC_TYPE_GAUGE")]
+    Gauge,
 }
 
-struct Sample {
-	EntityType: EntityType,
-	EntityName: String,
-	SampleType: SampleType,
-	SampleName: String,
-	MetricType: MetricType,
-	Value:      f64,
+#[derive(Copy, Clone, PartialEq, Serialize, Deserialize, Debug)]
+pub enum SampleType {
+    #[serde(rename = "")]
+    Invalid,
+    #[serde(rename = "SAMPLE_TYPE_ELECTRICITY_CONSUMPTION")]
+    ElectricityConsumption,
+    #[serde(rename = "SAMPLE_TYPE_ELECTRICITY_PRODUCTION")]
+    ElectricityProduction,
+    #[serde(rename = "SAMPLE_TYPE_GAS_CONSUMPTION")]
+    GasConsumption,
+    #[serde(rename = "SAMPLE_TYPE_TEMPERATURE")]
+    Temperature,
+    #[serde(rename = "SAMPLE_TYPE_PRESSURE")]
+    Pressure,
+    #[serde(rename = "SAMPLE_TYPE_FLOW")]
+    Flow,
+    #[serde(rename = "SAMPLE_TYPE_HUMIDITY")]
+    Humidity,
+    Time,
 }
 
-enum EntityType {
-  Invalid,
-  Tariff,
-  Zone,
-  Device
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct Measurement {
+    pub id: String,
+    pub source: String,
+    pub location: String,
+    pub samples: Vec<Sample>,
+    pub measured_at_time: DateTime<Utc>,
 }
 
-enum SampleType {
-  Invalid,
-  ElectricityConsumption,
-  ElectricityProduction,
-  GasConsumption,
-  Temperature,
-  Pressure,
-  Flow,
-  Humidity,
-  Time
-}
-
-enum MetricType {
-  Invalid,
-  Counter,
-  Gauge
+#[derive(Clone, Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct Sample {
+    pub entity_type: EntityType,
+    pub entity_name: String,
+    pub sample_type: SampleType,
+    pub sample_name: String,
+    pub metric_type: MetricType,
+    pub value: f64,
 }
