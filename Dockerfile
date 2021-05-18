f9eedaa7556c5e0b4abe2c5969f99908a515d83c
jorrit.salverda@gmail.com <jorrit.salverda@gmail.com>
2021-05-18 17:35:04 +0200

    build and test inside dockerfile; add extra sample types

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Step 1: Build the application
+FROM rust:1.52  as builder
+
+WORKDIR app
+
+# RUN apk add --update musl-dev
+RUN apt-get update && apt-get install -y --no-install-recommends musl-tools
+RUN rustup target add x86_64-unknown-linux-musl
+
+COPY . .
+RUN cargo test --release --target x86_64-unknown-linux-musl
+RUN ls -latr /app/target/x86_64-unknown-linux-musl/release/
+RUN cargo build --release --target x86_64-unknown-linux-musl
+RUN ls -latr /app/target/x86_64-unknown-linux-musl/release/
--- src/lib.rs
+++ src/lib.rs
@@ -33,15 +33,22 @@ pub enum SampleType {
     #[serde(rename = "SAMPLE_TYPE_ELECTRICITY_PRODUCTION")]
     ElectricityProduction,
     #[serde(rename = "SAMPLE_TYPE_GAS_CONSUMPTION")]
-    GasConsumption,
-    #[serde(rename = "SAMPLE_TYPE_TEMPERATURE")]
-    Temperature,
-    #[serde(rename = "SAMPLE_TYPE_PRESSURE")]
-    Pressure,
+    Energy,
     #[serde(rename = "SAMPLE_TYPE_FLOW")]
     Flow,
+    #[serde(rename = "SAMPLE_TYPE_ENERGY")]
+    GasConsumption,
+    #[serde(rename = "SAMPLE_TYPE_HEAT_DEMAND")]
+    HeatDemand,
     #[serde(rename = "SAMPLE_TYPE_HUMIDITY")]
     Humidity,
+    #[serde(rename = "SAMPLE_TYPE_PRESSURE")]
+    Pressure,
+    #[serde(rename = "SAMPLE_TYPE_TEMPERATURE")]
+    Temperature,
+    #[serde(rename = "SAMPLE_TYPE_TEMPERATURE_SETPOINT")]
+    TemperatureSetpoint,
+    #[serde(rename = "SAMPLE_TYPE_TIME")]
     Time,
 }
 
