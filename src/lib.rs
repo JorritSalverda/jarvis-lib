a43afdc9c10a1add1f4f7a5b2e7c211168d9613f
Jorrit Salverda <jorrit.salverda@gmail.com>
2021-05-20 10:50:13 +0200

    expose modules publicly

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
-mod bigquery_client;
-mod config_client;
-mod exporter_service;
-mod measurement_client;
-mod model;
-mod state_client;
+pub mod bigquery_client;
+pub mod config_client;
+pub mod exporter_service;
+pub mod measurement_client;
+pub mod model;
+pub mod state_client;
