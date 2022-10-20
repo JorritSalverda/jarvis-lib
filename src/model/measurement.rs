9a40fccaea23dc7078bf9e0caf45039d837eb56f
jorrit.salverda@gmail.com <jorrit.salverda@gmail.com>
2022-10-20 08:30:48 +0200

    cloneable measurement

--- src/model/measurement.rs
+++ src/model/measurement.rs
@@ -2,7 +2,7 @@ use crate::model::Sample;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "PascalCase")]
 pub struct Measurement {
     pub id: String,
